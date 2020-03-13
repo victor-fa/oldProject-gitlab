533c9a83ccb8a390fd7b135b2b75e75413504a88
827542599@qq.com <827542599@qq.com>
2020-03-13 15:31:33 +0800

    配置windows安装： 1、指定默认图标 2、指定默认项目名称 3、指定安装流程为可修改安装目录 4、创建桌面图标 5、支持32位安装使用

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,10 @@ module.exports = {
         "productName": "Nas-uGreen",//项目名，也是生成的安装文件名，即aDemo.exe
         "copyright": "Copyright © 2020",//版权信息
         "directories": {
-          "output": "./dist"//输出文件路径
+          "output": "./dist_electron"//输出文件路径
         },
         "win": {//win相关配置
-          "icon": "./logo.ico",//图标，当前图标在根目录下，注意这里有两个坑
+          "icon": "./logo.png",//图标，当前图标在根目录下，注意这里有两个坑
           "target": [
             {
               "target": "nsis",//利用nsis制作安装程序
@@ -19,6 +19,14 @@ module.exports = {
               ]
             }
           ]
+        },
+        "nsis": {
+          "oneClick": false, // 是否一键安装
+          "allowElevation": true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          "allowToChangeInstallationDirectory": true, // 允许修改安装目录
+          "createDesktopShortcut": true, // 创建桌面图标
+          "createStartMenuShortcut": true,// 创建开始菜单图标
+          "shortcutName": "Nas-uGreen", // 图标名称
         }
       }
     }
