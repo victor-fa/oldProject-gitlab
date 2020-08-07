ec7b6234c9c65b8254ca7bc0ca1a659648f7130f
827542599@qq.com <827542599@qq.com>
2020-08-07 09:25:52 +0800

    commit

--- src/store/modules/index.ts
+++
@@ -1,9 +0,0 @@
-let files = require.context('.', false, /\.ts$/)
-let modules: any = {}
-
-files.keys().forEach(key => {
-  if (key === './index.ts') return
-  modules[key.replace(/(\.\/|\.ts)/g, '')] = files(key).default
-})
-
-export default modules
